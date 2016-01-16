f53e01b67e8ef7155923fa1f32feb48eeab2dfb9
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2016-01-16 09:17:55 +0100

    utils.unmarshalAll added

--- utils.go
+++ utils.go
@@ -18,7 +18,6 @@ func byteToString(c []byte) string {
 	return string(c[:n+1])
 }
 
-
 func marshal(item interface{}, w http.ResponseWriter) {
 	bytes, err := json.Marshal(item)
 	if err != nil {
@@ -43,6 +42,22 @@ func unmarshal(r io.Reader, k string, w http.ResponseWriter) (payload map[string
 
 	if payload[k] == "" {
 		http.Error(w, "Please provide a '"+k+"'", http.StatusBadRequest)
+		return
+	}
+
+	return
+}
+
+func unmarshalAll(r io.Reader, w http.ResponseWriter) (payload interface{}) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(data, &payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
